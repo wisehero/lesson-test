92e3d37483db88db78d9bca5dffb71fd164aa42c
김지웅 <iop1996@easyshop.co.kr>
2025-07-23 15:52:39 +0900

    init

---
+++ src/main/java/com/easyshop/lessontest/LessonTestApplication.java
@@ -0,0 +1,13 @@
+package com.easyshop.lessontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easyshop/lessontest/LessonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyshop.lessontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
